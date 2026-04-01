8557db3230234d62a5e6106573906ee51d2ade97
Homam-Dany <dany.homam@etu.uae.ac.ma>
2026-04-01 03:38:28 +0100

    Fix NoReverseMatch and CSRF for local testing

--- school/school/settings.py
+++ school/school/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-pi$nglx)5#=a+helbr-^mls_nllzkuw%3n*#slnoawiqex1v&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
 
 
 # Application definition
